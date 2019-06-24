ffab749b46d3342f68d1dd819bae056ae4ee2450
LeBron-Jian <37261042+health-king@users.noreply.github.com>
2019-06-24 11:13:26 +0800

    Update settings.py

--- Book_project/settings.py
+++ Book_project/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'book_project',
         'USER': 'root',
-        'PASSWORD': 'wangjian',
+        'PASSWORD': '********',
         'HOST': 'localhost',
         'PORT': '3306',
     }
